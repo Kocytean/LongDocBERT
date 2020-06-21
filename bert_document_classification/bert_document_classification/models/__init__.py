4786903ab937fe6071943b72b6e58ec1fd4f12aa
Ayush Bahuguna <affirmation098@gmail.com>
2020-06-21 20:30:58 +0530

    Update __init__.py

--- bert_document_classification/bert_document_classification/models/__init__.py
+++ bert_document_classification/bert_document_classification/models/__init__.py
@@ -1,5 +1,1 @@
-from .obesity_document_bert import ObesityPhenotypingBert
-from .smoker_document_bert import SmokerPhenotypingBert
-from .newstest_document_bert import NewstestBert
-from .newstest_document_distilbert import NewstestDistilBert
 from .Generalised import Classifier
